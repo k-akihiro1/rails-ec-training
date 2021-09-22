f75f9b90f8602a14075cc940dbd61c3dd4515cb2
sugimoto <sugimoto.takumi@unizonbex.com>
2021-09-22 21:30:08 +0900

    #10 追加のバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
     belongs_to :user_classification
-    validates :password,    length: { in: 6..15 }
-    validates :last_name,    length: { maximum: 10 }
-    validates :first_name,    length: { maximum: 10 }
-    validates :zipcode, length: { is: 7 }
-    validates :prefecture,    length: { maximum: 5 }
-    validates :municipality,    length: { maximum: 10 }
-    validates :address,    length: { maximum: 15 }
-    validates :apartments,    length: { maximum: 20 }
-    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-    validates :phone_number,    length: { maximum: 15 }
+    with_options presence: true do
+        validates :password, format: { with: /\A[a-z0-9]+\z/i }, length: { in: 6..15 }
+        validates :last_name, length: { maximum: 10 }
+        validates :first_name, length: { maximum: 10 }
+        validates :zipcode, format: { with: /\A\d{3}[-]\d{4}\z/i }, length: { is: 7 }
+        validates :prefecture, length: { maximum: 5 }
+        validates :municipality, length: { maximum: 10 }
+        validates :address, length: { maximum: 15 }
+        validates :apartments, length: { maximum: 20 }
+        validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+        validates :phone_number, format: { with: /\A[a-z0-9]+\z/i }, length: { maximum: 15 }
 end
