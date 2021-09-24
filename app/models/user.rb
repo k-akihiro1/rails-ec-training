54346c9249da862d39d5a2e5a5a957085680e0e8
sugimoto <sugimoto.takumi@unizonbex.com>
2021-09-24 18:30:13 +0900

    #10 もろもろ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,16 @@
-class User < ApplicationRecord
-    with_options presence: true do
-      validates :password,  format: { with: /\A[a-z0-9]+\z/i },  length: { in: 6..15 }
-      validates :last_name,  length: { maximum: 10 }
-      validates :first_name,  length: { maximum: 10 }
-      validates :zipcode,  format: { with: /\A\d{3}[-]\d{4}\z/i },  length: { is: 7 }
-      validates :prefecture,  length: { maximum: 5 }
-      validates :municipality,  length: { maximum: 10 }
-      validates :address,  length: { maximum: 15 }
-      validates :apartments,  length: { maximum: 20 }
-      validates :email,  uniqueness: true,  format:  { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-      validates :phone_number,  format: { with: /\A\d+\z/ }, length: { maximum: 15 }
-    end
-<<<<<<< HEAD
-    belongs_to :user_classification
-=======
-    has_many :products
->>>>>>> 75b7b8297901d8ab3a815d4b35906202296756a8
-end
+  class User < ApplicationRecord
+  with_options presence: true do
+    validates :password,  format: { with: /\A[a-z0-9]+\z/i },  length: { in: 6..15 }
+    validates :last_name,  length: { maximum: 10 }
+    validates :first_name,  length: { maximum: 10 }
+    validates :zipcode,  format: { with: /\A\d{3}[-]\d{4}\z/i },  length: { is: 7 }
+    validates :prefecture,  length: { maximum: 5 }
+    validates :municipality,  length: { maximum: 10 }
+    validates :address,  length: { maximum: 15 }
+    validates :apartments,  length: { maximum: 20 }
+    validates :email,  uniqueness: true,  format:  { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :phone_number,  format: { with: /\A\d+\z/ }, length: { maximum: 15 }
+  end
+  belongs_to :user_classification
+  has_many :products
+  end
