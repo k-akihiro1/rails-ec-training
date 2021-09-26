10742c779199f31eb92b1d5fbc11c0bb289fee39
sugimoto <sugimoto.takumi@unizonbex.com>
2021-09-26 19:09:12 +0900

    zipcodeのlength条件変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
     validates :password, format: { with: /\A[a-z0-9]+\z/ }, length: { in: 6..15 }
     validates :last_name, length: { maximum: 10 }
     validates :first_name, length: { maximum: 10 }
-    validates :zipcode, format: { with: /\A\d{3}\d{4}\z/ }, length: { is: 8 }
+    validates :zipcode, format: { with: /\A\d{3}\d{4}\z/ }, length: { maximum: 8 }
     validates :prefecture, length: { maximum: 5 }
     validates :municipality, length: { maximum: 10 }
     validates :address, length: { maximum: 15 }
