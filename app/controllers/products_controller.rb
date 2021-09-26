0bab90bf008ebc25f0d021d3612a711868de2a53
sugimoto <sugimoto.takumi@unizonbex.com>
2021-09-26 21:58:56 +0900

    #21 productsコントーローラー作成、showアクションを追加

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,8 @@
+class ProductsController < ApplicationController
+  def show
+  end
+  def products_show
+  end
+  def products_show_not_found
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'products/show'
+  get 'products/products_show'
+  get 'products/products_show_not_found'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
