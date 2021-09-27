11a36d0f530065b7eb8a8909d30fbe6017ae992d
sugimoto <sugimoto.takumi@unizonbex.com>
2021-09-27 22:27:32 +0900

    routesを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'products/show'
-  get 'products/products_show'
-  get 'products/products_show_not_found'
+  get 'products/:id', to: 'products#products_show'
+  get 'products/', to: 'products#products_show_not_found'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
