133100d0cc9b6382c9abab4b8c8784f6ff480ad0
k_akihiro <k.shuhari@gmail.com>
2021-09-28 19:20:59 +0900

    #11 エラー対処

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,38 @@
+# Users
+UserClassification.create!(
+  user_classification_name: "通常会員"
+)
+
 User.create!(
   password: "1234abcd",
   last_name: "一郎",
   first_name: "テスト",
-  zipcode: "123-4567",
+  zipcode: "1234567",
   prefecture: "広島県",
   municipality: "中区幟町",
   address: "1-11",
   apartments: "幟町マンション　101号室",
-  email: "1234abcd@email",
-  phone_number: "090-1234-5678",
+  email: "1234abcd@example.com",
+binding.pry
+  phone_number: "0123456789",
   user_classification_id: 1,
   company_name: "一郎カンパニー",
   delete_flag: false
 )
-
 User.create!(
-  password: "1234ABCD",
+  password: "abcd1234",
   last_name: "二郎",
   first_name: "テスト",
-  zipcode: "987-6543",
+  zipcode: "9876543",
   prefecture: "東京都",
   municipality: "台東区",
   address: "1-11",
   apartments: "台東マンション　102号室",
-  email: "1234ABCD@email",
-  phone_number: "090-9876-5432",
+  email: "abcd1234@example.com",
+  phone_number: "987654321",
   user_classification_id: 1,
   company_name: "2郎カンパニー",
   delete_flag: false
-)
-
-CreateUserClassifications.create!(
-  user_classification_name: "通常会員"
 )
 
 # Peroducts
