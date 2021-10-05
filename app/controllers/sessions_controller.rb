f8e34c9596348c40056fa042f92d130f032f2664
sugimoto <sugimoto.takumi@unizonbex.com>
2021-10-05 21:35:01 +0900

    コンフリクトを解消

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,12 @@ class SessionsController < ApplicationController
       user = User.find_by(email: params[:session][:email].downcase)
       if user && user.authenticate(params[:session][:password])
         log_in user
-        redirect_to root_url
+        redirect_to "/login"
       else
         render 'new'
       end
 
       def destroy
         log_out if logged_in?
-        redirect_to root_url
       end
     end
