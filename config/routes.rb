c760b17c7c6fce41c332b09cc49baf96913b8e45
sugimoto <sugimoto.takumi@unizonbex.com>
2021-10-05 22:04:16 +0900

    routesの書き方を統一

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
-  post   'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   get 'products/show'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
