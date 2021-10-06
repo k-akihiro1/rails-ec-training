5d81a4090d543ae88a6f26ed08272786f5dc21fa
sugimoto <sugimoto.takumi@unizonbex.com>
2021-10-06 22:11:20 +0900

    検索結果の出し分けを実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,5 +5,7 @@ class ProductsController < ApplicationController
 
   def index
     @product = Product.all.page(params[:page]).per(8).order('created_at DESC')
+    # ↓はindex.htmlのuf文を機能させるための仮です
+    @product = Product.find_by(id: params[:id])
   end
 end
