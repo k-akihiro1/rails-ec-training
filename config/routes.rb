c89095002aab2c960f89dc73ae1605d9b37d1266
kanji fujii <gamu2134@gmail.com>
2021-10-06 22:30:52 +0900

    #13 必要ないファイルの中身を消去しました

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get 'user/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get    '/login',   to: 'sessions#new'
-  get 'products/show'
-end
