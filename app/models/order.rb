753ec3f30ed60abf70f0a0d0608575aab83cb530
k_akihiro <k.shuhari@gmail.com>
2021-10-13 23:24:08 +0900

    #23 発送状況の表示の更新

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,12 @@ class Order < ApplicationRecord
   def total_price
     order_details.sum(&:sub_tatal)
   end
+
+  def order_status
+    if order_details.any? {|order_detail| order_detail.shipment_status.shipment_status_name == "準備中"}
+      "準備中"
+    else
+      "発送中"
+    end
+  end
 end
