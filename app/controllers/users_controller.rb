b529699e3067ea909d321d20324983c747317367
k_akihiro <k.shuhari@gmail.com>
2021-10-20 00:50:21 +0900

    #25 ユーザー情報更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to user_path
+    else
+      flash[:error] = "不適切なURLです。"
+      render show
+    end
+  end
+
 end
