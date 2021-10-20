ebf5db85bf1c8ab1d5024ff7951f3eefa0361b3d
sugimoto <sugimoto.takumi@unizonbex.com>
2021-10-20 22:17:22 +0900

    検索ロジック追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,10 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all.page(params[:page]).per(15).order('created_at DESC')
   end
+
+  def search
+    @product = Product.search(params[:search])
+    @search = params[:search]
+    render "index"
+  end
 end
