c135983123d9ed5bce0a3bedaa639fc0b0b7c1dc
k_akihiro <k.shuhari@gmail.com>
2021-10-22 23:17:34 +0900

    #31 rootへの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root  'static_pages#home'
+  resources :orders
+  resources :users
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  resources :products
   get '/show', to: 'sessions#show'
   get '/index', to: 'sessions#index'
-    resources :orders
-    resources :users
-    get    '/login', to: 'sessions#new'
-    post   '/login', to: 'sessions#create'
-    delete '/logout', to: 'sessions#destroy'
-    resources :products
-  end
 end
