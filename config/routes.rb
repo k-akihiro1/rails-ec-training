bfc73a629f776452c70b243a02fb7208dc256dce
k_akihiro <k.shuhari@gmail.com>
2021-10-22 22:26:47 +0900

    #23 差分の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-  get    '/login', to: 'sessions#new'
-  post   '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
   get '/show', to: 'sessions#show'
   get '/index', to: 'sessions#index'
-  resources :products
-  resources :orders
-  resources :users
+    resources :orders
+    resources :users
+    get    '/login', to: 'sessions#new'
+    post   '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+    resources :products
+  end
 end
