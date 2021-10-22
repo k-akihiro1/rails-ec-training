1aafd1f5a4d677f063591833c79d5d631bab8d42
sugimoto <sugimoto.takumi@unizonbex.com>
2021-10-22 22:24:25 +0900

    不要部分を削除

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,12 +4,7 @@ class ProductsController < ApplicationController
   end
 
   def index
-    # @products = Product.all.page(params[:page]).per(15).order('created_at DESC')
     @products = Product.search(params[:search]).page(params[:page]).per(15).order('created_at DESC')
   end
 
-  def search
-    @search = params[:search]
-    render "index"
-  end
 end
