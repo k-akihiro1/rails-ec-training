0bc1396ebcfb8595eee128756755b97e22a6e0e0
k_akihiro <k.shuhari@gmail.com>
2021-10-22 21:00:13 +0900

    #23 flashの設定変更、ストロングパラメーターの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,27 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      redirect_to user_path
+      redirect_to @user
+      flash[:success] = "ユーザー情報の更新終了しました。"
     else
-      flash[:error] = "不適切なURLです。"
-      render show
+      flash[:error] = "ユーザー情報を更新できませんでした。。"
+      render 'edit'
     end
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :zipcode,
+      :prefecture,
+      :municipality,
+      :address,
+      :apartments,
+      :email,
+      :phone_number,
+      :password
+    )
   end
 
 end
