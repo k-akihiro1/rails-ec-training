554e66963ca91fc0233ae101bacc13c0e3d7423b
sugimoto <sugimoto.takumi@unizonbex.com>
2021-10-23 12:56:47 +0900

    インデントなどrubocopで修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,10 @@ class Product < ApplicationRecord
   belongs_to :sale_status
   belongs_to :category
   has_many :purchases, dependent: :destroy
-  def self.search(search,category_id)
+  def self.search(search, category_id)
     products = Product.all
-    products = products.where(['product_name LIKE ?', "%#{search}%"]) if search.present?
-    products = products.where(['category_id = ?', category_id]) if category_id.present?
+    products = products.where(["product_name LIKE ?", "%#{search}%"]) if search.present?
+    products = products.where(["category_id = ?", category_id]) if category_id.present?
     products
   end
 end
