209f2d286334a56557743633e53f531bb8c65861
k_akihiro <k.shuhari@gmail.com>
2021-10-24 13:35:04 +0900

    #32 ユーザー登録ページ、登録処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,35 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "登録完了しました。"
+      redirect_to @user
+    else
+      render 'new'
+      flash[:error] = "登録失敗しました。"
+    end
+
+  private
+
+    def user_params
+      params.require(:user).permit(
+        :first_name,
+        :last_name,
+        :zipcode,
+        :prefecture,
+        :municipality,
+        :address,
+        :apartments,
+        :email,
+        :phone_number,
+        :password
+      )
+    end
+  end
 end
