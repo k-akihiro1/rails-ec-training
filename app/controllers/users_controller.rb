a55a02b52ecda1a84fb89a66171000880176f50e
k_akihiro <k.shuhari@gmail.com>
2021-10-25 23:34:01 +0900

    #25 ユーザー更新機能の変更（作業中）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,28 +5,30 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      redirect_to @user
+    if @user.update(user_params)
       flash[:success] = "ユーザー情報の更新終了しました。"
+      redirect_to @user
     else
-      flash[:error] = "ユーザー情報を更新できませんでした。。"
+      flash[:error] = "ユーザー情報を更新できませんでした。"
       render :edit
     end
   end
 
-  def user_params
-    params.require(:user).permit(
-      :first_name,
-      :last_name,
-      :zipcode,
-      :prefecture,
-      :municipality,
-      :address,
-      :apartments,
-      :email,
-      :phone_number,
-      :password
-    )
-  end
+  private
+    def user_params
+      params.require(:user).permit(
+        :first_name,
+        :last_name,
+        :zipcode,
+        :prefecture,
+        :municipality,
+        :address,
+        :apartments,
+        :email,
+        :phone_number,
+        :password,
+        :password_confirmation
+      )
+    end
 
 end
