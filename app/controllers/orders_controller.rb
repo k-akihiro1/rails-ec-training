aa631d9d2851aba5ed309ecac1bb07450eb728a3
sugimoto <sugimoto.takumi@unizonbex.com>
2021-11-01 13:15:51 +0900

    order作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,8 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
+
+  def index
+    @orders = Order.page(params[:page]).per(15).order("created_at DESC")
+  end
 end
