511a541c5a9359dc113f056217643652fe02ac2c
k_akihiro <k.shuhari@gmail.com>
2021-11-26 22:36:21 -1000

    devise_forの削除、route.rbの整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root  'static_pages#home'
-  resources :orders
-  resources :users
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  resources :users
   resources :products
+  resources :orders
+
   post '/guest_login', to: 'static_pages#guest_login'
-  devise_for :users
+
 end
